901d78e798d29789ff9d8184f929e027397c1af3
alvinadl <231210067@student.mercubuana-yogya.ac.id>
2025-07-04 17:02:59 +0700

    laundry

--- App/Http/Controllers/OrderController.php
+++ App/Http/Controllers/OrderController.php
@@ -85,6 +85,6 @@ class OrderController extends Controller
     public function destroy(Order $order)
     {
         $order->delete();
-        return redirect()->route('orders.index')->with('success', 'Order berhasil dihapus!');
+        return redirect()->route('orders.index')->with('success', 'Order berhasil');
     }
 }
